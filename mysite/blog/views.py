74349803033297ff2b2fd3ed2662b4515e41389d
demor987 <jefertrade@gmail.com>
2024-06-10 19:33:02 +0300

    seo creating

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,14 +1,13 @@
-from django.shortcuts import render
-from blog.models import Post
-from django.http import Http404
+from django.shortcuts import render,  get_object_or_404
+from .models import Post
 
 def post_list(request):
     posts = Post.objects.all()
-    return render(request, 'blog/post/list.html', {'post': posts})
+    return render(request, 'blog/post/list.html', {'posts': posts})
 
-def post_detail(request, id):
-    try:
-        post = Post.objects.get(id=id)
-    except Post.DoesNotExist:
-        raise Http404('No post found.')
+def post_detail(request, year, month, day, slug):
+    post = get_object_or_404(Post, status=Post.Status.PUBLISHED,
+                             slug=post,
+                             publish__year=year,publish__month=month,publish__day=day
+                             )
     return render(request, 'blog/post/detail.html', {'post': post})
